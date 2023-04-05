6171462a8984031b53fe7483698bb64e5c53c502
WayneFerdon <wayneferdon@hotmail.com>
2023-04-05 08:07:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 # Author: wayneferdon wayneferdon@hotmail.com
 # Date: 2022-02-12 06:25:53
 # LastEditors: WayneFerdon wayneferdon@hotmail.com
-# LastEditTime: 2023-04-03 01:19:06
-# FilePath: \Wox.Base.Plugin.ChromeHistory\main.py
+# LastEditTime: 2023-04-05 01:55:49
+# FilePath: \Plugins\Wox.Base.Plugin.ChromeHistory\main.py
 # ----------------------------------------------------------------
 # Copyright (c) 2022 by Wayne Ferdon Studio. All rights reserved.
 # Licensed to the .NET Foundation under one or more agreements.
@@ -18,21 +18,21 @@ from WoxPluginBase_ChromeQuery import *
 
 from TimeStamp import *
 
-class GetHistory(ChromeQuery):
-    def _getDatas_(self):
+class ChromeHistoryQuery(ChromeQuery):
+    def __getDatas__(self):
         return ChromeCache.getHistories()
 
-    def _getResult_(self, regex:RegexList, data:History):
+    def __getResult__(self, regex:RegexList, data:History):
         item = f'{data.platform.name};{data.title};{stamp2time(data.lastVisitTime, "toMicroSec")};{data.url}/'
         if not regex.match(item):
             return
         subTitle = '[{time}] {url}'.format(time=stamp2time(data.lastVisitTime, 'toSec'), url=data.url)
-        return QueryResult(data.platform.name + ' ' + data.title,subTitle,data.icon,self._datas_.index(data),self._openUrl_.__name__,True,data.url).toDict()
+        return QueryResult(data.platform.name + ' ' + data.title,subTitle,data.icon,self.__datas__.index(data),self.openUrl.__name__,True,data.url).toDict()
 
-    def _extraContextMenu_(self, data: History):
+    def __extraContextMenu__(self, data: History):
         lastVisitTime = stamp2time(data.lastVisitTime, 'toMicroSec')
         return [self.getCopyDataResult('Last Visit Time', lastVisitTime, data.icon)]
 
 if __name__ == '__main__':
-    # GetHistory().query('')
-    GetHistory()
+    # QueryChromeHistory().query('')
+    ChromeHistoryQuery()
